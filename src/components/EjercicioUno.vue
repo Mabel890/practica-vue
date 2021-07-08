f2228eb9ac1e69d8013ceb09b9e3af85e039e1cd
Mabel890 <mabelvillada890@gmail.com>
2021-07-07 19:43:17 -0500

    Primer ejercicio

---
+++ src/components/EjercicioUno.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Esta es mi información: {{informacion}}</h1>
+        <input type="text" v-model="informacion"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                informacion: "",
+            }
+        }
+    }
+</script>
+
+<style></style>
