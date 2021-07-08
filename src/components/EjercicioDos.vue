80650d0e26cf3503c88f6665e11f77cfed74dca6
Mabel890 <mabelvillada890@gmail.com>
2021-07-07 20:21:22 -0500

    Segundo componente, suma

---
+++ src/components/EjercicioDos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Suma:</h1>
+        <input type="number" v-model="primerNumero">
+        <input type="number" v-model="segundoNumero">
+        <button v-on:click="sumar">Sumar</button>
+        <p>{{suma}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                suma: 0
+            };
+        },
+        methods: {
+            sumar(){
+                this.suma = parseInt(this.primerNumero) + parseInt(this.segundoNumero); 
+            }
+        }
+    };
+</script>
+
+<style></style>
