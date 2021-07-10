d0db67f5fd8f50d7302516cc862a8960070d3091
Mabel890 <mabelvillada890@gmail.com>
2021-07-10 07:56:06 -0500

    Ejercicio cuatro

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <EjercicioTres msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import EjercicioTres from './components/EjercicioTres.vue'
-
-export default {
-  name: 'App',
-  components: {
-    EjercicioTres
-  }
-}
-</script>
-<!-- Ejercicio 2, suma de dos valores
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <EjercicioDos msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import EjercicioDos from './components/EjercicioDos.vue'
-
-export default {
-  name: 'App',
-  components: {
-    EjercicioDos
-  }
-}
-</script>
--->
-
-<!-- Ejercicio 1
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <EjercicioUno msg="Welcome to Your Vue.js App"/>
+    <EjercicioDos msg="Welcome to Your Vue.js App"/>
+    <EjercicioTres msg="Welcome to Your Vue.js App"/>
+    <EjercicioCuatro msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import EjercicioUno from './components/EjercicioUno.vue'
+//import EjercicioUno from './components/EjercicioUno.vue'
+//import EjercicioDos from './components/EjercicioDos.vue'
+//import EjercicioTres from './components/EjercicioTres.vue'
+import EjercicioCuatro from './components/EjercicioCuatro.vue'
 
 export default {
   name: 'App',
   components: {
-    EjercicioUno
+    //EjercicioUno,
+    //EjercicioDos,
+    //EjercicioTres,
+    EjercicioCuatro
   }
 }
 </script>
--->
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
